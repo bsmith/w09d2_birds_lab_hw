32703c6f5b1312d43e6b49884cec0a1365d0b2f9
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-13 15:33:04 +0000

    Working create route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,35 +1,65 @@
 const express = require('express');
-const ObjectID = require('mongodb').ObjectID;
+/* NB. this isn't ObjectID as I accidentally updated to the new mongodb driver */
+const { ObjectId } = require('mongodb');
+
+const handleError = (err) => {
+    console.error(err);
+    res.status(500);
+    res.json({ status: 500, error: err });
+}
 
 const createRouter = function (collection) {
+    /* Setup CRUD routes */
+    const router = express.Router();
 
-  const router = express.Router();
+    /* INDEX */
+    router.get('/', (req, res) => {
+        collection
+            .find()
+            .toArray()
+            .then((docs) => res.json(docs))
+            .catch(handleError);
+    });
 
-  router.get('/', (req, res) => {
-    collection
-      .find()
-      .toArray()
-      .then((docs) => res.json(docs))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-      });
-  });
+    /* SHOW */
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+            .findOne({ _id: ObjectId(id) })
+            .then((doc) => res.json(doc))
+            .catch(handleError);
+    });
 
-  router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    collection
-      .findOne({ _id: ObjectID(id) })
-      .then((doc) => res.json(doc))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-      });
-  });
+    /* CREATE */
+    /* Again, due to accidentally updating mongodb this is different */
+    /* The return from insertOne basically just contains `insertedId` */
+    /* The documentation seems to imply that the id needs to be generated on the client-side anyway?! */
+    router.post('/', (req, res) => {
+        const newObject = req.body;
+        collection
+            .insertOne(newObject)
+            .then(({insertedId}) => {
+                /* two approaches here: just stuff the id into newObject, or do a fetch: maybe a redirect to the SHOW route?  Maybe a 201 with a Location header? */
+                console.log("created", insertedId);
+                res.json({...newObject, _id: insertedId});
+            })
+            .catch(handleError);
+    });
 
-  return router;
+    /* UPDATE */
+    router.put('/:id', (req, res) => {
+        const objId = new ObjectId(req.params.id);
+        const newObject = req.body;
+        throw new Error('stuff');
+    });
+
+    /* DESTROY */
+    router.delete('/:id', (req, res) => {
+        const objId = new ObjectId(req.param.id);
+        throw new Error('stuff');
+    });
+
+    return router;
 };
 
 module.exports = createRouter;
